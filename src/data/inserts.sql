a6db7dd04ddf548a281542d8d8b2bb56eebf9782
italo1033 <italogammy1033@gmail.com>
2022-07-21 02:31:30 -0300

    criando inserts

---
+++ src/data/inserts.sql
@@ -0,0 +1,45 @@
+use biblioteca;
+INSERT INTO livro(nome,idioma, autores, ip, df, tp, td, ta, resumo, isbn, nc, imageUrl)
+VALUES(
+'O pequeno principe',
+'Undetermined',
+'Antoine de Saint-Exupéry',
+'Global Editora 2020',
+'33 p.',
+'Livro; E-book',
+'Livro',
+'ClássicoMundial;,EternamenteResponsável;, Sentido da Vida',
+'é uma das obras mais conhecidas e mais vendidas de todos os tempos. Dotado de singular capacidade de imaginação, de inteligência e sabedoria, Antoine de Saint-Exupéry escreveu um livro que é uma profunda reflexão sobre o amor, a amizade e o sentido da vida.',
+'9786556120225',
+'uni.9786556120225',
+'https://images-na.ssl-images-amazon.com/images/I/41R4s%2Bz5FdL.jpg'
+);
+
+INSERT INTO livro(nome,idioma, autores, ip, df, tp, td, ta, resumo, isbn, nc, imageUrl)
+VALUES(
+'Harry Potter',
+'Portuguese',
+'Rowling, J. K.',
+'São Paulo Rocco 2008','--',
+'Livro',
+'Livro',
+'Braille Literatura infantojuvenil',
+'Em Harry Potter e as Relíquias da Morte, o encontro inevitável com Lord Voldemort não pode mais ser adiado. Harry, no entanto, precisa ganhar tempo para encontrar as Horcruxes que ainda estão faltando. E, pelo caminho, descobrir o que são afinal as Relíquias da Morte e como ele pode usá-las contra o Lorde das Trevas. Seguindo as poucas pistas deixadas por Dumbledore, Harry conta apenas com a ajuda dos leais amigos Rony e Hermione.Juntos, eles percorrem lugares nunca visitados, descobrem histórias nebulosas sobre pessoas queridas e acabam por desvendar mistérios que os incomodavam há muito tempo. Enquanto Harry, Rony e Hermione vagam por diferentes lugares em busca de pistas, J. K. Rowling vai revelando aspectos até então desconhecidos sobre os principais personagens.',
+'9788532522610',
+'cbu.70333',
+'http://3.bp.blogspot.com/-o5oaPzkaWM4/T2DOUrlO7zI/AAAAAAAAAQI/jwZrhE3KTxg/s1600/harry+potter+7.JPG');
+
+INSERT INTO livro(nome,idioma, autores, ip, df, tp, td, ta, resumo, isbn, nc, imageUrl)
+VALUES(
+'A branca de neve'
+'Portuguese',
+'Fagundes, Jacira',
+'São Paulo Fundação Dorina Nowill para Cegos 2014','83 il. color. Acompanha CD',
+'Livro',
+'Livro',
+'Livro',
+'Literatura infantojuvenil Literatura infantil',
+'Summary: A história de Branca de Neve recontada com muito humor. Uma Branca de Neve moderna, uma malévola mais humana e sete anões músicos e dançarinos revivem o conto clássico, agora em braille. Acompanha CD com áudio descrição.',
+'9788561257439',
+'cbu.137177',
+'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSrxlO7xidSrdKaQvpEG-UrN9pIiPwH6j4n40xoHnRUPjN6zNL9dAjboreThd9azJ4-Rp4Yiu4u_ay17wZx89RNfY4SWIEW98ODInhrwTtnRLnWr6gg6a_3&usqp=CAc');
